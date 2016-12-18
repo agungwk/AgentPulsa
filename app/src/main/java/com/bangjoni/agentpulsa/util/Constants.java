7a0135565a7a26d11298278b0fb030b10fbb775c
agung.kurniawan <spoor.badug@gmail.com>
2016-12-18 23:14:14 +0700

    - add map fragment

---
+++ app/src/main/java/com/bangjoni/agentpulsa/util/Constants.java
@@ -0,0 +1,11 @@
+package com.bangjoni.agentpulsa.util;
+
+/**
+ * Created by agung.kurniawan on 18-Dec-16.
+ */
+
+public class Constants {
+
+    public static final String BASE_URL = "http://139.59.244.156/merchantcommerce/";
+
+}
